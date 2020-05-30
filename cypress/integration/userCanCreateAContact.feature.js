1897b04699f5ec46f659990bc963eea914d75cca
odeane_croney <odeane_croney@yahoo.com>
2020-05-30 23:05:42 +0200

    Creating test for input elements then adding those elements to the index doc

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -7,6 +7,6 @@ describe('user can create a contact', () => {
         cy.get('#phone').type('0700 101010')
         cy.get('#company').type('craft Academy')
         cy.get('#notes').type('Awesome coder')
-        cy.get('#twitter').type('@croney')
+        cy.get('#twitter').type('@croney') 
     })
 })
